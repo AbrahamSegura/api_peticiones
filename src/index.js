f1eee88d22eb3360f5e819addea41ec8852b0fb3
Abraham Segura <abrahamdsm299@gmail.com>
2023-03-09 11:42:16 -0400

    agregando ruta para validar usuarios

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+import dotenv from 'dotenv'
+dotenv.config({ debug: true })
 import express from "express";
 import cors from "cors";
 const app = express()
 app.use(cors())
-
-app.get("/", function (_req, res) {
+app.use(express.json())
+app.get("/", (_req, res) => {
     res.send("hola")
 })
-const PORT = 3030
-app.listen(PORT, function () { console.log(`server listen on port: ${PORT}`) })
+app.post("/validUser", (req, res) => {
+    const { username, password } = req.body
+    console.log(username, password);
+    res.end
+})
+const PORT = process.env.PORT
+app.listen(PORT, () => { console.log(`server listen on port: ${PORT}`) })
