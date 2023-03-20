6e4cc347cdf6df4dda0b9fd33dc1c35d8444fe4e
Abraham Segura <abrahamdsm299@gmail.com>
2023-03-20 12:06:32 -0400

    update dev data base

---
+++ src/db/dbPrueba.js
@@ -0,0 +1,58 @@
+
+export const DB = {
+    "users": [
+        {
+            "id": "0",
+            "username": "abraham7",
+            "password": "3dd3ce298af9e2650a0ca9350d618bf8",
+            "departamento": "informatica",
+            "lista_peticiones": "",
+        },
+        {
+            "id": "1",
+            "username": "javier1",
+            "password": "b2af616a5c3ed4d2c6ed174138afe5dc",
+            "departamento": "informatica",
+            "lista_peticiones": "",
+        },
+        {
+            "id": "2",
+            "username": "mariana123",
+            "password": "142f7d70eb31d614e9850bc6efbc3f40",
+            "departamento": "administracion",
+            "lista_peticiones": 0,
+        },
+    ],
+    "lista_peticiones": [
+        {
+            "id": 0,
+            "lista": "0,1,2"
+        },
+    ],
+    "peticiones": [
+        {
+            "id": 0,
+            "descripcion": "no me abre el gmail",
+            "usuario": "mariana123",
+            "departamento": "administracion",
+            "estado": "en espera",
+            "fecha": new Date("2023", "1", "22")
+        },
+        {
+            "id": 1,
+            "descripcion": "no me abre el gmail",
+            "usuario": "mariana123",
+            "departamento": "administracion",
+            "estado": "en espera",
+            "fecha": new Date("2023", "2", "2")
+        },
+        {
+            "id": 2,
+            "descripcion": "no me abre el gmail",
+            "usuario": "mariana123",
+            "departamento": "administracion",
+            "estado": "en espera",
+            "fecha": new Date("2023", "2", "12")
+        },
+    ]
+}
